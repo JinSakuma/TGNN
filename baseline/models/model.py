8998903d2252832601d1dcc082a9ae16fe75e617
HayatoKTYM <hayato.katayama@ghelia.com>
2020-12-24 15:01:29 +0900

    first commit; baseline model

---
+++ baseline/models/model.py
@@ -0,0 +1,218 @@
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+class RMSLELoss(nn.Module):
+    """
+    Loss function :対数平均2乗誤差を定義
+    2020/05/13 : +1e-05 を追加 (lossが0になると微分不可になるため)
+    """
+    def __init__(self):
+        super().__init__()
+        self.mse = nn.MSELoss()
+        
+    def forward(self, pred, actual):
+        return torch.sqrt(self.mse(torch.log(pred + 1), torch.log(actual + 1))+1e-05)
+
+
+class Basemodel(nn.Module):
+    def __init__(self,
+                 mode=0,
+                 input_size=128,
+                 input_img_size=65,
+                 input_p_size=128,
+                 hidden_size=64,
+                 max_frame=60,
+                 ctc=True):
+        super(Basemodel, self).__init__()
+        """
+        mode: 0 _ VAD, 1 _ 画像, 2 _ 言語, 3 _ VAD+画像, 4 _ VAD+言語, 5 _ 画像+言語, 6 _ VAD+画像+言語
+        """
+        self.mode = mode
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.criterion = RMSLELoss()
+        self.max_frame = max_frame
+        self.r = 2.0
+
+        self.input_size = input_size
+        self.input_img_size = input_img_size
+        self.input_p_size = input_p_size
+
+        
+        self.hidden_size = hidden_size
+
+        # 音響LSTM
+        if self.mode in [0, 3, 4, 6]:
+            self.lstm_vad = torch.nn.LSTM(
+                input_size=input_size,
+                hidden_size=hidden_size,
+                batch_first=True,
+            )
+
+        # 画像LSTM
+        if self.mode in [1, 3, 5, 6]:
+            self.lstm_img = torch.nn.LSTM(
+                input_size=input_img_size,
+                hidden_size=hidden_size,
+                batch_first=True,
+            )
+
+        # 言語LSTM
+        if self.mode in [2, 4, 5, 6]:
+            self.lstm_lng = torch.nn.LSTM(
+                input_size=self.input_p_size,
+                hidden_size=hidden_size,
+                batch_first=True,
+            )
+
+        if self.mode == 0 or self.mode == 2:
+            self.fc = nn.Linear(hidden_size*2, 1)
+        elif self.mode == 1:
+            self.fc = nn.Linear(hidden_size, 1)
+        elif self.mode == 3 or self.mode == 5:
+            self.fc = nn.Linear(hidden_size*3, 1)
+        elif self.mode == 4:
+            self.fc = nn.Linear(hidden_size*4, 1)
+        else:
+            self.fc = nn.Linear(hidden_size*5, 1)
+
+        if self.mode in [2, 4, 5, 6]:
+            self.prev_hpa = torch.zeros(1, 64).to(self.device)
+            self.prev_hpb = torch.zeros(1, 64).to(self.device)
+            self.PAD = -1
+
+        self.hidden_size = hidden_size
+        self.hidden = None
+        self.hiddenA = None
+        self.hiddenB = None
+        self.hiddenPA = None
+        self.hiddenPB = None
+        self.hidden_img = None
+
+    def calc_voice(self, xA, xB):
+        hA, self.hiddenA = self.lstm_vad(xA, self.hiddenA)
+        hB, self.hiddenB = self.lstm_vad(xB, self.hiddenB)
+        return hA, hB
+
+    def calc_img(self, img):
+        hImg, self.hidden_img = self.lstm_img(img, self.hidden_img)
+        return hImg
+
+    def calc_lang(self, PA, PB):
+        hpA_list = []
+        hpB_list = []
+        for i in range(min(len(PA), len(PB))):
+            pa = PA[i]
+            pb = PB[i]
+            if len(pa) == 0:
+                hpA = self.prev_hpa
+            else:
+                if len(pa.shape) == 1:
+                    pa = pa.reshape(1, -1)
+                pa = torch.FloatTensor(pa).to(self.device)
+                pa = pa.unsqueeze(0)
+                hpA, self.hiddenPA = self.lstm_lng(pa, self.hiddenPA)
+                hpA = hpA[:, -1, :]
+            hpA_list.append(hpA)
+
+            if len(pb) == 0:
+                hpB = self.prev_hpb
+            else:
+                if len(pb.shape) == 1:
+                    pb = pb.reshape(1, -1)
+                pb = torch.FloatTensor(pb).to(self.device)
+                pb = pb.unsqueeze(0)
+                hpB, self.hiddenPB = self.lstm_lng(pb, self.hiddenPB)
+                hpB = hpB[:, -1, :]
+            hpB_list.append(hpB)
+
+            self.prev_hpa = hpA
+            self.prev_hpb = hpB
+
+        hpa = torch.cat(hpA_list)
+        hpb = torch.cat(hpB_list)
+        return hpa.unsqueeze(0), hpb.unsqueeze(0)
+
+    def calc_y(self, h):
+        y = torch.clamp(self.fc(h), min=0, max=60)
+        return y
+
+    def reset_state(self):
+        self.hiddenA = None
+        self.hiddenB = None
+        self.hiddenPA = None
+        self.hiddenPB = None
+        self.hidden_img = None
+
+    def back_trancut(self):
+        if self.hiddenA is not None:
+            self.hiddenA = (self.hiddenA[0].detach(), self.hiddenA[1].detach())
+            self.hiddenB = (self.hiddenB[0].detach(), self.hiddenB[1].detach())
+        if self.hidden_img is not None:
+            self.hidden_img = (self.hidden_img[0].detach(), self.hidden_img[1].detach())
+        if self.hiddenPA is not None:
+            self.hiddenPA = (self.hiddenPA[0].detach(), self.hiddenPA[1].detach())
+        if self.hiddenPB is not None:
+            self.hiddenPB = (self.hiddenPB[0].detach(), self.hiddenPB[1].detach())
+
+        if self.mode in [2, 4, 5, 6]:
+            self.prev_hpa = self.prev_hpa.detach()
+            self.prev_hpb = self.prev_hpb.detach()
+
+    def reset_phoneme(self):
+        self.prev_hpa = torch.zeros(1, 64).to(self.device)
+        self.prev_hpb = torch.zeros(1, 64).to(self.device)
+
+        
+
+
+    def forward(self, batch, phase='train'):
+        # 1つの入力の時系列の長さ
+        self.seq_size = batch['voiceA'].shape[0]
+
+        xA = torch.tensor(batch['voiceA']).to(self.device, dtype=torch.float32)
+        xA = xA.unsqueeze(0)
+        xB = torch.tensor(batch['voiceB']).to(self.device, dtype=torch.float32)
+        xB = xB.unsqueeze(0)
+        img = torch.tensor(batch['img']).to(self.device, dtype=torch.float32)
+        img = img.unsqueeze(0)
+        PA = batch['phonemeA']
+        PB = batch['phonemeB']
+        label = torch.tensor(batch['y']).to(self.device, dtype=torch.float32)
+
+
+        if self.mode in [0, 3, 4, 6]:
+            hA, hB = self.calc_voice(xA, xB)
+
+        if self.mode in [1, 3, 5, 6]:
+            hImg = self.calc_img(img)
+
+        if self.mode in [2, 4, 5, 6]:
+            hPA, hPB = self.calc_lang(PA, PB)
+
+        loss = 0
+        cnt = 0
+        # 特徴量のconcat
+        if self.mode == 0:
+            h = torch.cat([hA, hB], dim=-1)
+        elif self.mode == 1:
+            h = hImg
+        elif self.mode == 2:
+            h = torch.cat([hPA, hPB], dim=-1)
+        elif self.mode == 3:
+            h = torch.cat([hA, hB, hImg], dim=-1)
+        elif self.mode == 4:
+            h = torch.cat([hA, hB, hPA, hPB], dim=-1)
+        elif self.mode == 5:
+            h = torch.cat([hImg, hPA, hPB], dim=-1)
+        else:
+            h = torch.cat([hA, hB, hImg, hPA, hPB], dim=-1)
+
+        y = self.calc_y(h[:,-1])
+        loss = self.criterion(y, label)
+        if label < 60:             
+            loss = (loss * self.r)
+
+        return {'y': y.cpu().data.numpy(), 'loss': loss, 'cnt': cnt}
---
+++ baseline/utils/trainer.py
@@ -0,0 +1,176 @@
+import torch
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from sklearn.metrics import confusion_matrix, classification_report
+from utils.eval import quantitative_evaluation
+
+
+def train(net, mode, dataloaders_dict,
+          device, optimizer, scheduler
+          ):
+
+    #  dataloaders_dict['train'].on_epoch_end()
+    net.train()  # モデルを訓練モードに
+#     scheduler.step()
+#     print('lr:{}'.format(scheduler.get_lr()[0]))
+
+    epoch_loss = 0.0  # epochの損失和
+    train_cnt = 0
+    y_true, y_pred = np.array([]), np.array([])
+
+    for batch in tqdm(dataloaders_dict['train']):
+        net.reset_state()
+        if mode == 2 or mode >= 4:
+            net.reset_phoneme()
+
+        for i in range(len(batch[0])):
+            output_dict = net(batch[0][i])
+            
+            # y_true = np.append(y_true, 1 - batch[0][i]['y']//60)
+            # y_pred = np.append(y_pred, 1 - output_dict['y']//60)
+            loss = output_dict['loss']
+            if loss != 0 and loss != -1:
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                net.back_trancut()
+                loss = loss.item()
+
+            epoch_loss += loss
+            loss = 0
+            train_cnt += 1
+
+    epoch_loss = epoch_loss / train_cnt
+    output_dict = {}
+
+    # print(classification_report(y_true, y_pred))
+
+    return epoch_loss
+
+
+def val(net, mode, dataloaders_dict,
+        device, optimizer, scheduler,
+        epoch=10, output='./', resume=False,
+        only_eval=False
+        ):
+
+    #  dataloaders_dict['val'].on_epoch_end()
+    net.eval()  # モデルを訓練モードに
+    epoch_loss = 0.0  # epochの損失和
+    train_cnt = 0
+    y, y_pred = np.array([]), np.array([])
+    y_prob, u_list = np.array([]), np.array([])
+    with torch.no_grad():
+        for batch in tqdm(dataloaders_dict['val']):
+            net.reset_state()
+            if mode == 2 or mode >= 4:
+                    net.reset_phoneme()
+
+            for i in range(len(batch[0])):
+                output_dict = net(batch[0][i], phase='val')
+
+                y = np.append(y, batch[0][i]['y'])
+                # y_pred = np.append(y_pred, 1 - output_dict['y'] // 60)
+                y_prob = np.append(y_prob, output_dict['y'])
+
+                # print('batch u',batch[0][i]['u'])
+                u_list = np.append(u_list ,batch[0][i]['u'])
+
+                loss = output_dict['loss']
+                if loss != 0 and loss != -1:
+                    loss = loss.item()
+
+                epoch_loss += loss
+                loss = 0
+                train_cnt += 1
+
+
+        epoch_loss = epoch_loss / train_cnt
+
+    if only_eval:
+        return {
+            'y':y,
+            'y_pred':y_prob,
+            'u': u_list,
+            'loss':epoch_loss
+                }
+
+    torch.save(net.state_dict(), os.path.join(output, 'epoch_{}_loss_{:.3f}.pth'.format(epoch+1, epoch_loss)))
+
+    y_true = [1 if p < 60 else 0 for p in y]    
+    y_pred = np.zeros(len(y_true))
+
+    """
+    memo: 予測値が６０未満なら turn交代(1)と予測
+          (非発話区間の長さ) < (予測値) なら turn継続(0)と予測
+    """
+    timing_err = []
+    for j in range(len(y_true)):
+        if y_true[j]:
+            if y_prob[j] < 60:
+                y_pred[j] = 1
+                timing_err.append((y_prob[j] - y[j]) * 50)
+        else:
+            if y_prob[j] < u_list[j]:
+                y_pred[j] = 1
+    
+    print(classification_report(y_true, y_pred))
+    print(confusion_matrix(y_true, y_pred))
+    print('MAE: {} ms'.format(np.abs(timing_err).mean()))
+
+    fo = open(os.path.join(output, 'eval_report.txt'), 'a')
+    print('Epoch {}'.format(epoch+1), file=fo)
+    print(classification_report(y_true, y_pred),file=fo)
+    print('MAE: {} ms'.format(np.abs(timing_err).mean()), file=fo)
+    fo.close()
+    
+    return epoch_loss
+
+
+def trainer(net,
+            mode,
+            dataloaders_dict,
+            optimizer, scheduler,
+            num_epochs=10,
+            output='./',
+            resume=False,
+            only_eval=False
+            ):
+
+    os.makedirs(output, exist_ok=True)
+    Loss = {'train': [], 'val': []}
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('using', device)
+    net.to(device)
+
+    if not only_eval:
+        for epoch in range(num_epochs):
+            print('Epoch {}/{}'.format(epoch+1, num_epochs))
+            print('-------------')
+
+            for phase in ['train', 'val']:
+                print(phase)
+                # if phase == 'train' and epoch == 0: continue
+                if phase == 'train':
+                    epoch_loss = train(net, mode, dataloaders_dict, device, optimizer, scheduler)
+                else:
+                    epoch_loss = val(net, mode, dataloaders_dict, device,
+                                    optimizer, scheduler, epoch, output, resume)
+
+                print('{} Loss: {:.4f}'.format(phase, epoch_loss))
+                Loss[phase].append(epoch_loss)
+
+        if resume:
+            plt.figure(figsize=(15, 4))
+            plt.rcParams["font.size"] = 15
+            plt.plot(Loss['val'], label='val')
+            plt.plot(Loss['train'], label='train')
+            plt.legend()
+            plt.savefig(os.path.join(output, 'history.png'))
+    else:
+        output = val(net, mode, dataloaders_dict, device,
+                    optimizer, scheduler, 0, output, resume, only_eval=only_eval)
+
+        return output
